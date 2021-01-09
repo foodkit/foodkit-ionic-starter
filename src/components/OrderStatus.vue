7c75c4b1210357508f5082676e0a3914c335f8fb
Corey Mcmahon <contact@coreymcmahon.com>
2021-01-09 22:35:42 +0700

    [ch0] fix issue with order tracking page

--- src/components/OrderStatus.vue
+++ src/components/OrderStatus.vue
@@ -1,19 +1,50 @@
 <template>
   <div>
     <div class="wrapper">
-      <ion-img :src="stateImg" />
-      <p class="status">{{ stateFormatted }}</p>
+      <ion-img :src="image" />
+      <p class="status">{{ description }}</p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { computed, defineComponent, inject, ref } from "vue";
+  import { computed, defineComponent } from "vue";
   import { IonImg } from "@ionic/vue";
 
-  import OrdersService from "@/services/ordersService";
-  import ApiClient from "@/services/apiClient";
-  import AuthService from "@/services/authService";
+  const statusToString = (status: string) => {
+    switch (status) {
+      case "unverified":
+      case "pending":
+        return "Awaiting approval...";
+      case "vendor-accepted":
+      case "driver-accepted":
+        return "Food is being cooked";
+      case "dispatched":
+        return "On the way";
+      case "completed":
+        return "Completed";
+      case "cancelled":
+        return "Order Cancelled";
+    }
+    return 'unknown';
+  };
+
+  const statusToImage = (status: string) => {
+    switch (status) {
+      case "unverified":
+      case "pending":
+      case "vendor-accepted":
+        return "/assets/order-in-progress.svg";
+      case "driver-accepted":
+      case "dispatched":
+        return "/assets/order-on-the-way.svg";
+      case "completed":
+        return "/assets/order-completed.svg";
+      case "cancelled":
+        return "/assets/order-cancelled.svg";
+    }
+    return "/assets/order-cancelled.svg";
+  };
 
   export default defineComponent({
     name: "OrderStatus",
@@ -21,63 +52,15 @@
       IonImg,
     },
     props: {
-      orderId: {
-        type: Number,
+      status: {
+        type: String,
         required: true,
       },
     },
-    async setup(props) {
-      const authService = inject<AuthService>("authService");
-      if (authService === undefined) {
-        throw new Error("Auth service is not bound.");
-      }
-
-      const state = ref<string | null>(null);
-
-      const ordersService = new OrdersService(new ApiClient(), authService);
-
-      state.value = await ordersService.status(props.orderId);
-
-      setInterval(async () => {
-        state.value = await ordersService.status(props.orderId);
-      }, 10000);
-
-      const stateFormatted = computed(() => {
-        switch (state.value) {
-          case "unverified":
-          case "pending":
-            return "Awaiting approval...";
-          case "vendor-accepted":
-          case "driver-accepted":
-            return "Food is being cooked";
-          case "dispatched":
-            return "On the way";
-          case "completed":
-            return "Completed";
-          case "cancelled":
-            return "Order Cancelled";
-        }
-      });
-
-      const stateImg = computed(() => {
-        switch (state.value) {
-          case "unverified":
-          case "pending":
-          case "vendor-accepted":
-            return "/assets/order-in-progress.svg";
-          case "driver-accepted":
-          case "dispatched":
-            return "/assets/order-on-the-way.svg";
-          case "completed":
-            return "/assets/order-completed.svg";
-          case "cancelled":
-            return "/assets/order-cancelled.svg";
-        }
-      });
-
+    setup(props) {
       return {
-        stateFormatted,
-        stateImg,
+        description: computed(() => statusToString(props.status)),
+        image: computed(() => statusToImage(props.status)),
       };
     },
   });
--- src/views/OrderStatusScreen.vue
+++ src/views/OrderStatusScreen.vue
@@ -6,26 +6,14 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Order Status</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <Suspense>
-        <template #default>
-          <OrderStatus :order-id="id" />
-        </template>
-        <template #fallback>
-          Loading order information...
-        </template>
-      </Suspense>
+      <OrderStatus v-if="status" :status="status" />
+      <div v-if="!status">Loading order information...</div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+  import { defineComponent, inject, ref } from "vue";
   import {
     IonPage,
     IonHeader,
@@ -33,9 +21,26 @@
     IonTitle,
     IonContent,
   } from "@ionic/vue";
+  import OrdersService from "@/services/ordersService";
+  import ApiClient from "@/services/apiClient";
+  import AuthService from "@/services/authService";
 
   import OrderStatus from "@/components/OrderStatus.vue";
   import { useRoute } from "vue-router";
+
+  // We need to make sure the setInterval call is only triggered while the user is on the order tracking page
+  // and is only triggered ONCE per session. Do this by keeping track of the interval ID and freeing it when
+  // user navigates to a different page.
+  let interval: any = null;
+  const tickBus = {
+    listener: () => console.log('no-op'),
+    tick: function () {
+      this.listener && this.listener();
+    },
+    listen: function (listener: any) {
+      this.listener = listener;
+    },
+  };
 
   export default defineComponent({
     name: "OrderStatusScreen",
@@ -47,11 +52,30 @@
       IonPage,
       OrderStatus,
     },
-    setup() {
+    ionViewWillEnter() {
+      interval = setInterval(() => {
+        tickBus.tick();
+      }, 10000);
+    },
+    ionViewWillLeave() {
+      clearInterval(interval);
+    },
+    setup () {
       const route = useRoute();
-      const id: number = (route.params.id as unknown) as number;
-
-      return { id };
+      const id = (route.params.id as unknown) as number;
+      const authService = inject<AuthService>("authService");
+      if (authService === undefined) {
+        throw new Error("Auth service is not bound.");
+      }
+      const ordersService = new OrdersService(new ApiClient(), authService);
+      const status = ref<null|string>(null);
+      const update = () => {
+        ordersService.status(id)
+          .then((nextStatus) => status.value = nextStatus);
+      };
+      tickBus.listen(update); // trigger the update once per tick (every 10 seconds)
+      update(); // force the update once when the page loads
+      return { status };
     },
   });
 </script>
